092dafa097e0bbae2297304f23b6af8eeab49aa5
Steve McClain <steve@marmalade.co>
2024-11-18 15:39:12 -0500

    Add statically generated path as a test

---
+++ app/static-params/[slug]/page.tsx
@@ -0,0 +1,11 @@
+export function generateStaticParams() {
+  return [{ slug: "hello%20world" }]
+}
+
+export default async function Slug2Page(props: { params: Promise<{ slug: string }> }) {
+  const { slug } = await props.params;
+  console.log({ path: "static-params", slug });
+  return (
+    <p>{slug}</p>
+  )
+}
